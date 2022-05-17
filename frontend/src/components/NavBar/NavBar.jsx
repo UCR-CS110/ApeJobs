df5832ddf8226df06f17d3e667ec28a33d00d243
Kent Phan <kentphan2000@gmail.com>
2022-05-17 13:58:07 -0700

    Add suggestions

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,26 +1,33 @@
-import * as React from "react";
-import {
-	AppBar,
-	Typography,
-	Box,
-	Toolbar,
-	Button,
-} from "@mui/material";
+import React from "react";
+import { AppBar, Typography, Box, Toolbar, Button } from "@mui/material";
 import { User } from "tabler-icons-react";
-import { Link } from 'react-router-dom';
-import "./NavBar.css"
+import { Link } from "react-router-dom";
+import "./NavBar.css";
 
+// Put either in appjs so all pages have it or on every page that needs it
 export const NavBar = () => {
 	return (
 		<Box sx={{ flexGrow: 1 }}>
-			<AppBar className="navbar" position="static" sx={{ bgcolor: "white" }}>
+			<AppBar
+				className="navbar"
+				position="static"
+				sx={{ bgcolor: "white" }}
+			>
 				<Toolbar>
-				<Typography
+					<Typography
+						component={Link}
+						to="/"
 						variant="h5"
 						edge="start"
 						color="black"
 						aria-label="menu"
-						sx={{ mr: 2 , fontSize: "35px", fontFamily: "Inter", fontWeight:"1000"}}
+						sx={{
+							mr: 2,
+							fontSize: "35px",
+							fontFamily: "Inter",
+							fontWeight: "1000",
+              textDecoration: 'none'
+						}}
 					>
 						ApeJobs
 					</Typography>
@@ -29,9 +36,8 @@ export const NavBar = () => {
 						variant="h6"
 						component="div"
 						sx={{ flexGrow: 1 }}
-					>
-					</Typography>
-					<Button component={Link} to="/signin" color="inherit" >
+					></Typography>
+					<Button component={Link} to="/profile" color="inherit">
 						<User size={35} strokeWidth={1.5} color={"black"} />
 					</Button>
 				</Toolbar>
