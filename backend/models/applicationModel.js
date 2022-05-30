371f629a5f1e1d57da76f4893412b3a09efd4217
Kent Phan <kentphan2000@gmail.com>
2022-05-30 07:04:45 -0700

    add application endpoints

--- backend/models/applicationModel.js
+++ backend/models/applicationModel.js
@@ -3,18 +3,22 @@ const { Schema } = mongoose;
 
 const applicationSchema = new Schema(
 	{
-		author: { type: Schema.Types.ObjectId, ref: 'User'},
-		title: String,
-		interests: [String],
-		majors: [String],
-		description: String,
-		people: Number,
-		skills: [String],
-		pay: String,
-		questions: [String],
-		
+		user: {
+			userId: {
+				type: Schema.Types.ObjectId,
+				ref: "User",
+			},
+			name: String,
+		},
+		// embedding applicant responses, unless we want to make another model for it
+		optionalFields: [{ question: String, answer: String, _id: false }],
+		//! Alternative
+		// optionalFields: [{ type: Schema.Types.ObjectId, ref: "Message" }],
+		job: { type: Schema.Types.ObjectId, ref: "Job" },
+		status: String,
+		messages: [{ type: Schema.Types.ObjectId, ref: "Message" }]
 	},
 	{ timestamps: true }
 );
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("Application", applicationSchema);
---
+++ backend/routes/applicationRoutes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+	getApplications,
+	getApplicationById,
+	setApplication,
+	updateApplication,
+	deleteApplication,
+	getMessages,
+	sendMessage,
+} = require("../controllers/applicationController");
+
+// add logic in a function in controllers, export and call in routes
+
+router.route("/").get(getApplications).post(setApplication);
+router
+	.route("/:id")
+	.get(getApplicationById)
+	.put(updateApplication)
+	.delete(deleteApplication);
+// TODO: may be a better way to route this but idk how
+router.route("/:id/messages").get(getMessages).post(sendMessage);
+
+module.exports = router;
