1d277f66757cf620f97aaff238079493f5e1df97
Kent Phan <kentphan2000@gmail.com>
2022-06-01 10:27:12 -0700

    add job listing creation page

--- frontend/src/pages/JobListing/JobListing.jsx
+++ frontend/src/pages/JobListing/JobListing.jsx
@@ -1,148 +1,171 @@
+import CreatableSelect from "react-select/creatable";
 import axios from "axios";
-import React from "react";
-import { JobCard } from "../../components/Home/JobCard/JobCard";
+import { useReducer, useState } from "react";
 import {
 	TextField,
-	Select,
-	FormGroup,
-	FormHelperText,
-	MenuItem,
-	Button,
-	FormControlLabel,
-	FormLabel,
-	Chip,
-	Grid,
 	Typography,
-	Checkbox,
+	Box,
+	CardContent,
 	Card,
+	Container,
+	Paper,
+	Button,
+	FormControl,
 } from "@mui/material";
+import { majors } from "../../constants/majors";
+import { SquarePlus } from "tabler-icons-react";
 
-// {
-// 	author: {
-// 		userId: {
-// 			type: Schema.Types.ObjectId,
-// 			ref: "User",
-// 		},
-// 		name: String,
-// 	},
-// 	title: String,
-// 	interests: [String],
-// 	majors: [String],
-// 	skills: [String],
-// 	pay: String,
-// 	questions: [String],
-// 	applications: [{ type: Schema.Types.ObjectId, ref: "Application" }],
-// }
+// TODO: put somewhere else later
+const options = [
+	{ value: "Computer Science", label: "Computer Science" },
+	{ value: "Computer Engineering", label: "Computer Engineering" },
+	{ value: "Electrical Engineering", label: "Electrical Engineering" },
+	{ value: "Statistics", label: "Statistics" },
+	{ value: "Mathematics", label: "Mathematics" },
+	{ value: "Data Science", label: "Data Science" },
+	{
+		value: "Computer Science w/ Business Applications",
+		label: "Computer Science w/ Business Applications",
+	},
+	{ value: "Physics", label: "Physics" },
+];
+
+const reducer = (state, action) => {
+	switch (action.type) {
+		case "set":
+			return { count: state.count + action.payload };
+		default:
+			throw new Error();
+	}
+};
 
 export const JobListing = () => {
+	// TODO: figure out how to use this
+	// const [state, dispatch] = useReducer(reducer, initialState);
+
+	const [course, setCourse] = useState("");
+	const [position, setPosition] = useState("");
+	const [desc, setDesc] = useState("");
+	const [pay, setPay] = useState(null);
+	const [questions, setQuestions] = useState([]);
+	const [majors, setMajors] = useState([]);
+	const [interests, setInterests] = useState([]);
+	const [skills, setSkills] = useState([]);
+
+	const handleChange = (e, setState) => {
+		setState(e.target.value);
+		console.log(e.target.value);
+	};
+
+	const onSubmit = () => {};
 	return (
 		<>
-			<Card sx={{ boxShadow: 6 }}>
-				<Grid
-					container
-					spacing={3}
-					justifyContent="center"
-					alignItems="center"
+			<Container component="main" maxWidth="md" sx={{ mb: 4 }}>
+				<Paper
+					sx={{
+						boxShadow: 6,
+						my: { xs: 3, md: 6 },
+						p: { xs: 2, md: 3 },
+					}}
 				>
-					<Grid item xs={12} sm={6}>
+					<Box
+						component="form"
+						sx={{ display: "flex", flexDirection: "column" }}
+					>
 						<TextField
-							required
-							id="firstName"
-							name="firstName"
-							label="First name"
-							fullWidth
-							autoComplete="given-name"
-							variant="standard"
+							sx={{ mb: 2 }}
+							label="Course"
+							variant="outlined"
+							value={course}
+							// onChange={() => dispatch({ type: 'set', payload: 1 })}
+							onChange={(e) => handleChange(e, setCourse)}
 						/>
-					</Grid>
-					<Grid item xs={12} sm={6}>
 						<TextField
-							required
-							id="lastName"
-							name="lastName"
-							label="Last name"
-							fullWidth
-							autoComplete="family-name"
-							variant="standard"
+							sx={{ mb: 2 }}
+							label="Position"
+							variant="outlined"
+							value={position}
+							onChange={(e) => handleChange(e, setPosition)}
 						/>
-					</Grid>
-					<Grid item xs={12}>
 						<TextField
-							required
-							id="address1"
-							name="address1"
-							label="Address line 1"
-							fullWidth
-							autoComplete="shipping address-line1"
-							variant="standard"
+							sx={{ mb: 2 }}
+							label="Description"
+							rows={5}
+							multiline
+							value={desc}
+							onChange={(e) => handleChange(e, setDesc)}
 						/>
-					</Grid>
-					<Grid item xs={12}>
 						<TextField
-							id="address2"
-							name="address2"
-							label="Address line 2"
-							fullWidth
-							autoComplete="shipping address-line2"
-							variant="standard"
+							sx={{ mb: 2 }}
+							label="Hourly Pay"
+							type="number"
+							variant="outlined"
+							value={pay}
+							onChange={(e) => handleChange(e, setPay)}
 						/>
-					</Grid>
-					<Grid item xs={12} sm={6}>
-						<TextField
-							required
-							id="city"
-							name="city"
-							label="City"
-							fullWidth
-							autoComplete="shipping address-level2"
-							variant="standard"
-						/>
-					</Grid>
-					<Grid item xs={12} sm={6}>
-						<TextField
-							id="state"
-							name="state"
-							label="State/Province/Region"
-							fullWidth
-							variant="standard"
-						/>
-					</Grid>
-					<Grid item xs={12} sm={6}>
-						<TextField
-							required
-							id="zip"
-							name="zip"
-							label="Zip / Postal code"
-							fullWidth
-							autoComplete="shipping postal-code"
-							variant="standard"
-						/>
-					</Grid>
-					<Grid item xs={12} sm={6}>
-						<TextField
-							required
-							id="country"
-							name="country"
-							label="Country"
-							fullWidth
-							autoComplete="shipping country"
-							variant="standard"
-						/>
-					</Grid>
-					<Grid item xs={12}>
-						<FormControlLabel
-							control={
-								<Checkbox
-									color="secondary"
-									name="saveAddress"
-									value="yes"
+
+						{/* Make so that you can add any amount of question textfields */}
+						<Typography variant="h6" align="left" sx={{ m: 1 }}>
+							Optional Questions For Applicant
+						</Typography>
+						<Box
+							display="flex"
+							justifyContent="flex-end"
+							alignItems="flex-end"
+						>
+							<TextField
+								fullWidth
+								rows={5}
+								value={questions}
+								onChange={(e) => handleChange(e, setQuestions)}
+							/>
+							<Button variant="contained" sx={{ ml: 1 }}>
+								<SquarePlus
+									size={45}
+									strokeWidth={1}
+									color={"white"}
 								/>
-							}
-							label="Use this address for payment details"
+							</Button>
+						</Box>
+						<Typography variant="h6" align="left" sx={{ m: 1 }}>
+							Majors
+						</Typography>
+						<CreatableSelect
+							closeMenuOnSelect={false}
+							isMulti
+							options={options}
+							// value={majors}
+							// onChange={(e) => setMajors([...majors, e])}
 						/>
-					</Grid>
-				</Grid>
-			</Card>
+						<Typography variant="h6" align="left" sx={{ m: 1 }}>
+							Interests
+						</Typography>
+						<CreatableSelect
+							closeMenuOnSelect={false}
+							isMulti
+							value={interests}
+							onChange={(e) => console.log(e.target)}
+						/>
+						<Typography variant="h6" align="left" sx={{ m: 1 }}>
+							Skills
+						</Typography>
+						<CreatableSelect
+							closeMenuOnSelect={false}
+							isMulti
+							value={skills}
+							onChange={(e) => console.log(e)}
+						/>
+					</Box>
+					<Button
+						variant="contained"
+						size="large"
+						sx={{ mt: 3 }}
+						onSubmit={onSubmit}
+					>
+						Create Listing
+					</Button>
+				</Paper>
+			</Container>
 		</>
 	);
 };
