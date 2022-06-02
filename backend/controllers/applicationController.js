a5f1b66f55eae4727ad93c533b91a5730a74e55b
Kent Phan <kentphan2000@gmail.com>
2022-06-02 05:35:21 -0700

    add accept or deny buttons and endpoints

--- backend/controllers/applicationController.js
+++ backend/controllers/applicationController.js
@@ -13,7 +13,8 @@ const getApplications = asyncHandler(async (req, res) => {
 	} else if (userId) {
 		// idk if i should put this here
 		// faster way
-		apps = (await User.findById(userId).populate("applications")).applications;
+		apps = (await User.findById(userId).populate("applications"))
+			.applications;
 		// apps = await Application.find({ "user.userId": userId });
 	} else {
 		apps = await Application.find();
@@ -30,14 +31,16 @@ const getApplicationById = asyncHandler(async (req, res) => {
 	} else
 		await Application.findOne({ _id: req.params.id })
 			.lean()
+			.populate("user.userId")
+			.exec()
 			.then((app) => res.status(200).json(app));
 });
 
 const getApplicationByJobId = asyncHandler((req, res) => {
-  Application.find({ job: req.params.job_id }, (err, apps) => {
-    if (err) return res.status(400).send("No apps founds.");
-    res.json(apps);
-  });
+	Application.find({ job: req.params.job_id }, (err, apps) => {
+		if (err) return res.status(400).send("No apps founds.");
+		res.json(apps);
+	});
 });
 
 // TODO: if an app for exists for the current user, do not let them create one
@@ -51,74 +54,79 @@ const getApplicationByJobId = asyncHandler((req, res) => {
 // 	job: ObjectId,
 // }
 const setApplication = asyncHandler(async (req, res) => {
-  const apps = await Application.create(
-    {
-      user: req.body.user, // user ref
-      optionalFields: req.body.optionalFields,
-      job: req.body.job, // job ref
-      status: req.body.status,
-      messages: [],
-    },
-    (err, app) => {
-      if (err || !app || !app.job)
-        return res.status(400).send("Could not add app.");
-      Job.findOneAndUpdate(
-        { _id: app.job },
-        { $push: { applications: app._id } },
-        (err, job) => {
-          if (err || !job) return res.status(400).send("Could not find job.");
-        }
-      );
-	  User.findOneAndUpdate(
-        { _id: req.body.user.userId },
-        { $push: { applications: app._id } },
-        (err, usr) => {
-          if (err || !usr) return res.status(400).send("Could not find user.");
-        }
-      );
-	  return res.status(200).json(app);
-    }
-  );
+	const apps = await Application.create(
+		{
+			user: req.body.user, // user ref
+			optionalFields: req.body.optionalFields,
+			job: req.body.job, // job ref
+			status: req.body.status,
+			messages: [],
+		},
+		(err, app) => {
+			if (err || !app || !app.job)
+				return res.status(400).send("Could not add app.");
+			Job.findOneAndUpdate(
+				{ _id: app.job },
+				{ $push: { applications: app._id } },
+				(err, job) => {
+					if (err || !job)
+						return res.status(400).send("Could not find job.");
+				}
+			);
+			User.findOneAndUpdate(
+				{ _id: req.body.user.userId },
+				{ $push: { applications: app._id } },
+				(err, usr) => {
+					if (err || !usr)
+						return res.status(400).send("Could not find user.");
+				}
+			);
+			return res.status(200).json(app);
+		}
+	);
 });
 
 const updateApplication = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `update app ${req.params.id}` });
+	const app = await Application.findByIdAndUpdate(req.params.id, req.body);
+	// res.status(200).json({ message: `update app ${req.params.id}` });
+	res.status(200).json(app);
 });
 
 const deleteApplication = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `delete app ${req.params.id}` });
+  await Application.findByIdAndDelete(req.params.id)
+	res.status(200).json({ message: `delete app ${req.params.id}` });
 });
 
 const getMessages = asyncHandler(async (req, res) => {
-  console.log(req.params.id);
-  Application.findOne({ _id: req.params.id })
-    .populate("messages")
-    .exec(function (err, app) {
-      res.status(200).json(app.messages);
-    });
+	console.log(req.params.id);
+	Application.findOne({ _id: req.params.id })
+		.populate("messages")
+		.exec(function (err, app) {
+			res.status(200).json(app.messages);
+		});
 });
 
 const sendMessage = asyncHandler(async (req, res) => {
-  const msg = await Message.create({
-    user: req.body.user, // user ref
-    message: req.body.message,
-    application: req.body.application, // app ref
-  });
+	const msg = await Message.create({
+		user: req.body.user, // user ref
+		message: req.body.message,
+		application: req.body.application, // app ref
+	});
 
-  await Application.findOneAndUpdate(
-    { _id: req.body.application },
-    { $push: { messages: msg._id } }
-  );
-  res.status(200).json(msg);
+	await Application.findOneAndUpdate(
+		{ _id: req.body.application },
+		{ $push: { messages: msg._id } }
+	);
+	res.status(200).json(msg);
 });
 
 module.exports = {
-  getApplications,
-  getApplicationById,
-  getApplicationByJobId,
-  setApplication,
-  updateApplication,
-  deleteApplication,
-  getMessages,
-  sendMessage,
+	getApplications,
+	getApplicationById,
+	getApplicationByJobId,
+	setApplication,
+	updateApplication,
+	deleteApplication,
+	getMessages,
+	sendMessage,
 };
--- frontend/src/pages/CaseStatus/CaseStatus.jsx
+++ frontend/src/pages/CaseStatus/CaseStatus.jsx
@@ -1,240 +1,317 @@
 import React from "react";
 import {
-  Button,
-  Box,
-  Container,
-  Divider,
-  FormControl,
-  Grid,
-  IconButton,
-  List,
-  ListItem,
-  ListItemText,
-  Paper,
-  TextField,
-  Typography,
-  Avatar,
+	Button,
+	Box,
+	Container,
+	Divider,
+	FormControl,
+	Grid,
+	IconButton,
+	List,
+	ListItem,
+	ListItemText,
+	Paper,
+	TextField,
+	Typography,
+	Avatar,
+	Card,
 } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import { backendUrl } from "../../constants/backendUrl";
 import { UserContext } from "../../contexts/UserContext/UserContext";
 import { useLocation } from "react-router-dom";
 import { JobInfo } from "../../components/Application/JobInfo/JobInfo";
+import { StudentCard } from "../../components/StudentCard/StudentCard";
 import axios from "axios";
 
 const StyledPaper = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(3),
-  color: theme.palette.text.primary,
-  paddingRight: theme.spacing(2),
-  boxShadow: 6,
+	backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+	...theme.typography.body2,
+	padding: theme.spacing(3),
+	color: theme.palette.text.primary,
+	paddingRight: theme.spacing(2),
+	boxShadow: 6,
 }));
 
 const Comment = ({ message, picture }) => {
-  return (
-    <>
-      <Grid my={"1em"} mr={"1em"} item xs={12}>
-        <StyledPaper>
-          <Grid container wrap="nowrap" spacing={2}>
-            <Grid item>
-              <Avatar src={picture}></Avatar>
-            </Grid>
-            <Grid item xs>
-              <Typography>{message.message}</Typography>
-            </Grid>
-          </Grid>
-        </StyledPaper>
-      </Grid>
-    </>
-  );
+	return (
+		<>
+			<Grid my={"1em"} mr={"1em"} item xs={12}>
+				<StyledPaper>
+					<Grid container wrap="nowrap" spacing={2}>
+						<Grid item>
+							<Avatar src={picture}></Avatar>
+						</Grid>
+						<Grid item xs>
+							<Typography>{message.message}</Typography>
+						</Grid>
+					</Grid>
+				</StyledPaper>
+			</Grid>
+		</>
+	);
 };
 
 const CommentContainer = ({ app, application }) => {
-  const [messages, setMessages] = React.useState(application.messages);
-  const [userMessage, setUserMessage] = React.useState("");
-  const { _id, picture, name } = React.useContext(UserContext);
+	const [messages, setMessages] = React.useState(application.messages);
+	const [userMessage, setUserMessage] = React.useState("");
+	const { _id, picture, name } = React.useContext(UserContext);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const msg = {
-      user: { userId: _id, picture, name },
-      message: userMessage,
-      application: application._id,
-    };
-    console.log("going ot send", msg);
-    axios
-      .post(`${backendUrl}/api/applications/${app._id}/messages`, msg)
-      .then((res) => {
-        // setError();
-        console.log("sent msg", res.data);
-        setMessages((prev) => [...prev, res.data]);
-      })
-      .catch((e) => {
-        console.log(e);
-        // setError(
-        // 	"Unable to complete your request. Please try again later."
-        // );
-      });
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const msg = {
+			user: { userId: _id, picture, name },
+			message: userMessage,
+			application: application._id,
+		};
+		console.log("going ot send", msg);
+		axios
+			.post(`${backendUrl}/api/applications/${app._id}/messages`, msg)
+			.then((res) => {
+				// setError();
+				console.log("sent msg", res.data);
+				setMessages((prev) => [...prev, res.data]);
+			})
+			.catch((e) => {
+				console.log(e);
+				// setError(
+				// 	"Unable to complete your request. Please try again later."
+				// );
+			});
 
-    setUserMessage("");
-  };
+		setUserMessage("");
+	};
 
-  React.useEffect(() => {
-    axios
-      .get(`${backendUrl}/api/applications/${app._id}/messages`)
-      .then((res) => {
-        console.log("msgs", res.data);
-        setMessages(res.data);
-        // setError();
-        //console.log(res.data);
-      })
-      .catch((e) => {
-        console.log(e);
-        // setError(
-        // 	"Unable to complete your request. Please try again later."
-        // );
-      });
-  }, [app]);
+	React.useEffect(() => {
+		axios
+			.get(`${backendUrl}/api/applications/${app._id}/messages`)
+			.then((res) => {
+				console.log("msgs", res.data);
+				setMessages(res.data);
+				// setError();
+				//console.log(res.data);
+			})
+			.catch((e) => {
+				console.log(e);
+				// setError(
+				// 	"Unable to complete your request. Please try again later."
+				// );
+			});
+	}, [app]);
 
-  return (
-    <>
-      {application && (
-        <Paper elevation={5}>
-          <Box sx={{ boxShadow: 4 }} p={3}>
-            <Grid container direction="row" alignItems="center">
-              <Grid item xs={12}>
-                <Typography variant="h4" gutterBottom>
-                  Comments
-                </Typography>
-                <Divider />
-              </Grid>
-              <Grid item xs={12}>
-                <Grid container m="1em" alignItems="center">
-                  <Grid
-                    sx={{ height: "30rem", overflowY: "scroll" }}
-                    xs={12}
-                    item
-                  >
-                    {messages.map((message, index) => (
-                      <Comment
-                        key={index}
-                        message={message}
-                        picture={message.user && message.user.picture}
-                      />
-                    ))}
-                  </Grid>
-                  <Grid xs={9} mt={"1em"} item>
-                    <form className="user-message" onSubmit={handleSubmit}>
-                      <TextField
-                        label="Type your message..."
-                        value={userMessage}
-                        onChange={(event) => {
-                          setUserMessage(event.target.value);
-                        }}
-                        variant="outlined"
-                        required
-                        fullWidth
-                      />
-                    </form>
-                  </Grid>
-                  <Grid xs={3} item>
-                    <Button
-                      type="submit"
-                      disabled={!userMessage}
-                      onClick={handleSubmit}
-                      variant="contained"
-                    >
-                      Send
-                    </Button>
-                  </Grid>
-                </Grid>
-              </Grid>
-            </Grid>
-          </Box>
-        </Paper>
-      )}
-    </>
-  );
+	return (
+		<>
+			{application && (
+				<Paper elevation={5}>
+					<Box sx={{ boxShadow: 4 }} p={3}>
+						<Grid container direction="row" alignItems="center">
+							<Grid item xs={12}>
+								<Typography variant="h4" gutterBottom>
+									Comments
+								</Typography>
+								<Divider />
+							</Grid>
+							<Grid item xs={12}>
+								<Grid container m="1em" alignItems="center">
+									<Grid
+										sx={{
+											height: "30rem",
+											overflowY: "scroll",
+										}}
+										xs={12}
+										item
+									>
+										{messages.map((message, index) => (
+											<Comment
+												key={index}
+												message={message}
+												picture={
+													message.user &&
+													message.user.picture
+												}
+											/>
+										))}
+									</Grid>
+									<Grid xs={9} mt={"1em"} item>
+										<form
+											className="user-message"
+											onSubmit={handleSubmit}
+										>
+											<TextField
+												label="Type your message..."
+												value={userMessage}
+												onChange={(event) => {
+													setUserMessage(
+														event.target.value
+													);
+												}}
+												variant="outlined"
+												required
+												fullWidth
+											/>
+										</form>
+									</Grid>
+									<Grid xs={3} item>
+										<Button
+											type="submit"
+											disabled={!userMessage}
+											onClick={handleSubmit}
+											variant="contained"
+										>
+											Send
+										</Button>
+									</Grid>
+								</Grid>
+							</Grid>
+						</Grid>
+					</Box>
+				</Paper>
+			)}
+		</>
+	);
 };
 
 export const Question = ({ question }) => {
-  return (
-    <>
-      <Box my={"1em"}>
-        <Typography inline variant="body1" align="left">
-          <Box sx={{ fontWeight: "bold" }}>{question.question}</Box>
-        </Typography>
-        <Typography inline variant="body1" align="left">
-          {question.answer}
-        </Typography>
-      </Box>
-    </>
-  );
+	return (
+		<>
+			<Box my={"1em"}>
+				<Typography inline variant="body1" align="left">
+					<Box sx={{ fontWeight: "bold" }}>{question.question}</Box>
+				</Typography>
+				<Typography inline variant="body1" align="left">
+					{question.answer}
+				</Typography>
+			</Box>
+		</>
+	);
 };
 
 export const QuestionsCard = ({ job }) => {
-  return (
-    <>
-      <Paper elevation={5}>
-        <Box sx={{ boxShadow: 4 }} p={3}>
-          <Typography inline variant="h5" align="left">
-            <Box sx={{ fontWeight: "bold" }}>Questions</Box>
-          </Typography>
-          {job.optionalFields.map((question, index) => (
-            <Question question={question} />
-          ))}
-        </Box>
-      </Paper>
-    </>
-  );
+	return (
+		<>
+			<Paper elevation={5}>
+				<Box sx={{ boxShadow: 4 }} p={3}>
+					<Typography inline variant="h5" align="left">
+						<Box sx={{ fontWeight: "bold" }}>Questions</Box>
+					</Typography>
+					{job.optionalFields.map((question, index) => (
+						<Question question={question} />
+					))}
+				</Box>
+			</Paper>
+		</>
+	);
 };
 
 export const CaseStatus = () => {
-  const [application, setApplication] = React.useState();
-  const [job, setJob] = React.useState();
+	const [application, setApplication] = React.useState();
+	const [job, setJob] = React.useState();
+	const user = React.useContext(UserContext);
 
-  const location = useLocation();
-  React.useEffect(() => {
-    //regex to remove %20 that appears in names with an apostrophe
-    axios
-      .get(`${backendUrl}/api/applications/${location.state._id}`)
-      .then((res) => {
-        setApplication(res.data);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-    axios
-      .get(`${backendUrl}/api/jobs/?jobId=${location.state.job}`)
-      .then((res) => {
-        console.log("job", res.data);
-        setJob(res.data);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  }, [setApplication, location]);
+	const location = useLocation();
+	React.useEffect(() => {
+		//regex to remove %20 that appears in names with an apostrophe
+		axios
+			.get(`${backendUrl}/api/applications/${location.state._id}`)
+			.then((res) => {
+				setApplication(res.data);
+			})
+			.catch((e) => {
+				console.log(e);
+			});
+		axios
+			.get(`${backendUrl}/api/jobs/?jobId=${location.state.job}`)
+			.then((res) => {
+				console.log("job", res.data);
+				setJob(res.data);
+			})
+			.catch((e) => {
+				console.log(e);
+			});
+	}, [setApplication, location]);
 
-  return (
-    <>
-      <Grid container>
-        <Grid ml={4} mr={"4em"} mt={"1em"} item xs={4}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              {application && job && <JobInfo job={job} />}
-            </Grid>
-            <Grid item xs={12}>
-              {application && <QuestionsCard job={application} />}
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid mt={"1em"} item xs={7}>
-          {application && (
-            <CommentContainer app={location.state} application={application} />
-          )}
-        </Grid>
-      </Grid>
-    </>
-  );
+	// TODO: fix this unless its okay to do
+	const handleDecision = (decision) => {
+		const appId = location.state._id;
+		axios
+			.put(`${backendUrl}/api/applications/${appId}`, {
+				status: decision,
+			})
+			.then((res) => {
+				console.log(res.data);
+			})
+			.catch((e) => {
+				console.log(e);
+			});
+	};
+
+	return (
+		<>
+			<Grid container>
+				<Grid ml={4} mr={"4em"} mt={"1em"} item xs={4}>
+					<Grid container spacing={2}>
+						<Grid item xs={12}>
+							{application && user.type === "student"
+								? job && <JobInfo job={job} />
+								: application && (
+										<Card sx={{ boxShadow: 6 }}>
+											<Box
+												sx={{
+													display: "flex",
+													flexDirection: "row",
+													mt: 3,
+													justifyContent: "center",
+												}}
+											>
+												<Button
+													sx={{ mx: 2 }}
+													variant="contained"
+													color="success"
+													onClick={() =>
+														handleDecision(
+															"accepted"
+														)
+													}
+												>
+													Accept
+												</Button>
+												<Button
+													sx={{
+														mx: 2,
+														minWidth: "6rem",
+													}}
+													variant="contained"
+													color="error"
+													onClick={() =>
+														handleDecision(
+															"denied"
+														)
+													}
+												>
+													Deny
+												</Button>
+											</Box>
+											<StudentCard
+												user={application.user.userId}
+											/>
+										</Card>
+								  )}
+							{application && console.log(application)}
+						</Grid>
+						<Grid item xs={12}>
+							{application && <QuestionsCard job={application} />}
+						</Grid>
+					</Grid>
+				</Grid>
+				<Grid mt={"1em"} item xs={7}>
+					{application && (
+						<CommentContainer
+							app={location.state}
+							application={application}
+						/>
+					)}
+				</Grid>
+			</Grid>
+		</>
+	);
 };
