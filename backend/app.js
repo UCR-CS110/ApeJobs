e6fb1d74fe88d735200e790909d7abe3b1cf353c
Kent Phan <kentphan2000@gmail.com>
2022-06-10 21:31:26 -0700

    heroku deployment

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,19 @@
 var express = require("express");
 var cors = require("cors");
-const dotenv = require('dotenv').config();
+const path = require("path")
+const dotenv = require("dotenv").config();
 const app = express();
 const connectDB = require("./config/db");
 const userManagement = require("./routes/userManagement.js");
 const jobRoutes = require("./routes/jobRoutes.js");
 const applicationRoutes = require("./routes/applicationRoutes");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 
-connectDB(); 
+connectDB();
 
-app.use(
-  cors({credentials: true, origin: 'http://localhost:3000'})
-);
+const PORT = process.env.PORT || 80;
+
+app.use(cors({ credentials: true, origin: `http://localhost:${PORT}` }));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -21,8 +22,13 @@ app.use("/api/jobs", jobRoutes);
 //! TODO: make private or something
 app.use("/api/applications", applicationRoutes);
 
-app.listen(80, function () {
-  console.log("CORS-enabled web server listening on port 80");
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../frontend/build")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+	});
+}
+
+app.listen(PORT, function () {
+	console.log(`CORS-enabled web server listening on port ${PORT}`);
 });
-
-
